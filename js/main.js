70df06f656413de60554e1f8091e41a3e52932ed
KimGeonah <gunnnna@naver.com>
2023-07-04 14:45:10 +0900

    ver06

--- js/main.js
+++ js/main.js
@@ -5,6 +5,46 @@ window.onscroll = function() {
   console.log(scrollTop);
 }  
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
+ //[자바스크립트] 풀페이지 기능 ----------------------------------------------
+
+$(function(){
+
+  $('.wrap section').mousewheel(function(event, delta){
+
+      if(delta > 0) {//스크롤을 위로 움직임
+          let prev = $(this).prev().offset().top
+          $('html, body').stop().animate({
+              scrollTop:prev
+          },500)
+      }else if(delta <0){//스크롤을 아래로 움직임
+          let next = $(this).next().offset().top
+          $('html, body').stop().animate({
+              scrollTop:next
+          },500)
+      }
+
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -31,6 +71,10 @@ $(function(){
         },1000)
       })
     })
+
+
+
+
   
 
 
@@ -54,10 +98,14 @@ $(function(){
 
 
 
+
+
+
 //[바닐라] intro page : 타이핑효과  -----------------------------------------------
 
+
 const typed = new Typed('.typing', {
-  strings: ['&lt;h1&gt; Hello! World &lt;/h1&gt;'],
+  strings: ['&lt;h1&gt; Geonah World &lt;/h1&gt;'],
   typeSpeed: 100,
   loop: true,
 });
@@ -118,6 +166,27 @@ tapMenu.forEach((menu, index) => {
 });
 
 
+//[바닐라] about 취미 : swiper  -----------------------------------------------
+
+
+var hobbySwiper = new Swiper(".hobby_swiper", {
+  observer:true,
+  observeParents:true,
+  slidesPerView: 3,
+  autoplay: {
+    delay:300,
+    disableOnInteraction: false
+  },
+  initialSlide:0,
+  spaceBetween: 20,
+
+  
+});
+
+
+
+
+
 //[바닐라] 프로필 : swiper  -----------------------------------------------
 
  const swiper = new Swiper('.swiper', {
@@ -129,12 +198,12 @@ tapMenu.forEach((menu, index) => {
   
    loop:true,
 
-   //자동 재생 여부, 
+    //자동 재생 여부, 
   autoplay:{
-    delay: 1000,
+    delay: 1500,
     // 드래그로 슬라이더 넘겼을 때 autoplay가 멈추지 않는다.
     disableOnInteraction: false  } 
-
+  
 
 
 
@@ -158,13 +227,15 @@ $('.swiper').hover(function() {
 
 $(window).scroll(function() {
   let scrollTop = $(window).scrollTop();
-  if (scrollTop >= 5200) {
+  if (scrollTop >= 4965) {
     $("#progress01").animate({ width: "90%" }, 1500);
     $("#progress02").animate({ width: "90%" }, 1500);
     $("#progress03").animate({ width: "70%" }, 1500);
     $("#progress04").animate({ width: "40%" }, 1500);
     $("#progress05").animate({ width: "70%" }, 1500);
     $("#progress06").animate({ width: "90%" }, 1500);
+  } else {
+    $("#progress01, #progress02, #progress03, #progress04, #progress05, #progress06").stop().css("width", "0%");
   }
 });
 
@@ -184,71 +255,88 @@ $(document).ready(function() {
 });
 
 
-//[제이쿼리] works : 모달창   -----------------------------------------------
+//[제이쿼리] works : 탭1 모달창1   -----------------------------------------------
 
 
-const url01 = document.querySelector('.url_01')
-const url02 = document.querySelector('.url_02')
-
-const modal01 = document.querySelector('#modal_01')
-const modal02 = document.querySelector('#modal_02')
-
-const back01 = document.querySelector('#background_01')
-const back02 = document.querySelector('#background_02')
-
-const close01= document.querySelector('#close_01')
-const close02= document.querySelector('#close_02')
 
 const body = document.querySelector('body');
 
+
+const url01 = document.querySelector('.url_01')
+const modal01 = document.querySelector('#modal_01')
+const back01 = document.querySelector('#background_01')
+const close01= document.querySelector('#close_01')
+
+
+//기획서 버튼 클릭시 작동되는 탭
 url01.addEventListener('click', function() {
   modal01.style.display = 'block';
   back01.style.display = 'block';
   body.style.overflow = 'hidden';
 });
-url02.addEventListener('click', function() {
-  modal02.style.display = 'block';
-  back02.style.display = 'block';
-  body.style.overflow = 'hidden';
-});
 
+
+//기획서 닫기 클릭시 작동
 close01.addEventListener('click', function() {
   modal01.style.display = 'none';
   back01.style.display = 'none';
   body.style.overflowY = 'auto';
 
   //나갔다 들어올때 active 첫번재 이동
-  $('.modal_menu .menu').removeClass('active');
-  $('.modal_menu .menu').eq(0).addClass('active');
-  $('.modal_tap').css('display', 'none').removeClass('active');
-  $('.modal_tap').eq(0).css('display', 'block').addClass('active');
+  $('#modal_01 .modal_menu .menu').removeClass('active');
+  $('#modal_01 .modal_menu .menu').eq(0).addClass('active');
+  $('#modal_01 .modal_tap').css('display', 'none').removeClass('active');
+  $('#modal_01 .modal_tap').eq(0).css('display', 'block').addClass('active');
 });
+
+
+
+
+//[제이쿼리] works : 탭1 모달창2   -----------------------------------------------
+const modal02 = document.querySelector('#modal_02')
+const url02 = document.querySelector('.url_02')
+const back02 = document.querySelector('#background_02')
+const close02= document.querySelector('#close_02')
+
+
+
+
+//코드리뷰 버튼 클릭시 작동되는 탭
+url02.addEventListener('click', function() {
+  modal02.style.display = 'block';
+  back02.style.display = 'block';
+  body.style.overflow = 'hidden';
+});
+
+
+
+//코드리뷰 닫기 클릭시 작동
 close02.addEventListener('click', function() {
   modal02.style.display = 'none';
   back02.style.display = 'none';
   body.style.overflowY = 'auto';
 
     //나갔다 들어올때 active 첫번재 이동
-  $('.modal_menu .menu').removeClass('active');
-  $('.modal_menu .menu').eq(0).addClass('active');
-  $('.modal_tap').css('display', 'none').removeClass('active');
-  $('.modal_tap').eq(0).css('display', 'block').addClass('active');
+  $('#modal_02 .modal_menu .menu').removeClass('active');
+  $('#modal_02 .modal_menu .menu').eq(0).addClass('active');
+  $('#modal_02 .modal_tap').css('display', 'none').removeClass('active');
+  $('#modal_02 .modal_tap').eq(0).css('display', 'block').addClass('active');
 });
 
 
 
 
 
-//[바닐라] 모달창 : 탭메뉴 -----------------------------------------------
+//[바닐라] 모달창1 : 탭메뉴 -----------------------------------------------
 
 
 
 
  //모달 메뉴를 클릭할때
-$('.modal_menu .menu').click(function() {
+$('#modal_01 .modal_menu .menu').click(function() {
 
   //메뉴전체에서 일단 active를 지우고
-  $('.modal_menu .menu').removeClass('active');
+  $('#modal_01 .modal_menu .menu').removeClass('active');
   //선택된 메뉴만 active시킨다
   $(this).addClass('active');
 
@@ -256,9 +344,10 @@ $('.modal_menu .menu').click(function() {
   let index = $(this).index();
 
 
-  $('.modal_tap').css('display', 'none').removeClass('active');
-  $('.modal_tap').eq(index).css('display', 'block').addClass('active');
+  $('#modal_01 .modal_tap').css('display', 'none').removeClass('active');
+  $('#modal_01 .modal_tap').eq(index).css('display', 'block').addClass('active');
   
+
   //탭메뉴의 스와이퍼 첫페이지시작
   if (index == 0) {
     swiper01.slideTo(0);
@@ -273,13 +362,42 @@ $('.modal_menu .menu').click(function() {
 
 
 
-//[바닐라] 모달창 : swiper  -----------------------------------------------
+
+//[바닐라] 모달창2 : 탭메뉴 -----------------------------------------------
+
+
+
+
+ //모달 메뉴를 클릭할때
+ $('#modal_02 .modal_menu .menu').click(function() {
+
+  //메뉴전체에서 일단 active를 지우고
+  $('#modal_02 .modal_menu .menu').removeClass('active');
+  //선택된 메뉴만 active시킨다
+  $(this).addClass('active');
+
+  //그리고 선택된 메뉴에 해당하는 인덱스를 찾아서
+  let index = $(this).index();
+
+
+  $('#modal_02 .modal_tap').css('display', 'none').removeClass('active');
+  $('#modal_02 .modal_tap').eq(index).css('display', 'block').addClass('active');
+
+});
+
+
+
+
+
+
+//[바닐라] 탭메뉴1번 모달창 : swiper 3개  -----------------------------------------------
 
 
 
 
 
 var swiper01 = new Swiper(".swiper01", {
+  initialSlide:0,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
